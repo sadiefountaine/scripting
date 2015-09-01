4702d27dbdc43fbadf02f40040022e88fedbe56c
Sadie Fountaine <sadiefountaine@live.com>
2015-09-01 09:48:01 -0600

    Variables and Functions Script

---
+++ scripting_1600/Assets/VariablesandFunctions.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VariablesAndFunctions : MonoBehaviour {
+
+	int example = 7;
+	// Use this for initialization
+	//This is a comment. Leaving notes is good. The application won't try to run this.
+	void Start () 
+	{
+		Debug.Log (example);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
