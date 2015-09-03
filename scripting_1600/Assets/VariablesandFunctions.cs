5a031c872b0d1ac4e13327050702d6717e1d001b
Sadie Fountaine <sadiefountaine@live.com>
2015-09-03 09:42:43 -0600

    V and F Practice Scripts

--- scripting_1600/Assets/VariablesandFunctions.cs
+++ scripting_1600/Assets/VariablesandFunctions.cs
@@ -1,18 +1,26 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class VariablesAndFunctions : MonoBehaviour {
+public class VariablesAndFunctions : MonoBehaviour 
+{
 
-	int example = 7;
+	int example = 15;
 	// Use this for initialization
 	//This is a comment. Leaving notes is good. The application won't try to run this.
 	void Start () 
 	{
+		//example = MultiplyByFive (example);
+		example = DivideByFive (example);
 		Debug.Log (example);
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
+	//void Update ()
+	//int MultiplyByFive (int number)
+	int DivideByFive (int number)
+	{
+		int ret;
+		ret = number / 5;
+		return ret;
 	}
 }
