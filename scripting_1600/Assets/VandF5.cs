36ce612cdca09a807eb5128d00170ed8ce88219a
Sadie Fountaine <sadiefountaine@live.com>
2015-09-08 08:52:19 -0600

    Signed Off Variables and Functions Stuff

---
+++ scripting_1600/Assets/VandF5.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VandF5 : MonoBehaviour 
+{
+	int hopscotch = 25;
+	// Use this for initialization
+	void Start () 
+	{
+		hopscotch = DivideByFive (hopscotch);
+		Debug.Log (hopscotch);
+	}
+	int DivideByFive (int number)
+	{
+		int ret;
+		ret = number / 5;
+		return ret;
+	}
+
+	// Update is called once per frame
+
+	//void Update () 
+	
+}
