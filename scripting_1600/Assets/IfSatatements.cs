41d707b5cc782fdda32feae8359da44251d29d71
unknown <10740294@CS514-13.ad.uvu.edu>
2015-09-22 09:28:46 -0600

    no message

---
+++ scripting_1600/Assets/IfSatatements.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IfSatatements : MonoBehaviour
+{
+	public float CarSpeed = 102;
+	public float TooSlow = 15;
+
+	void Update ()
+	{
+		if (Input.GetKeyDown (KeyCode.Space))
+			TemperatureTest ();
+		CarSpeed -= Time.deltaTime * 2;
+	}
+
+	void TemperatureTest ()
+	{
+		// Car speed less than too slow.
+		if (CarSpeed < TooSlow) 
+		{
+			print ("You shouldn't be on the road at this speed.");
+		}
+		// Car speed if it isn't too slow.
+		else 
+		{
+			print ("You're going to get pulled over");
+		}
+	}
+}
--- scripting_1600/Assets/conventionsandsyntaxpassoff.cs
+++ scripting_1600/Assets/conventionsandsyntaxpassoff.cs
@@ -15,7 +15,7 @@ public class conventionsandsyntaxpassoff : MonoBehaviour
 	void Update()
 	{
 		//The following line of code is used to track the position on the falling cube. Enter the correct convention.
-		Debug.Log (transform.position.x);
+		Debug.Log (transform.position.y);
 
 		/*The following statement will tell unity when to print "I'm going to splater!" to the console if the cube is < or = 10. 
 		Enter the correct statement.
