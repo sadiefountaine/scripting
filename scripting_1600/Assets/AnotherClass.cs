462d58aa7fb3466081984109f8e6cb010928cd03
Sadie Fountaine <sadiefountaine@live.com>
2015-10-06 08:42:08 -0600

    Scope and Access Modifiers

---
+++ scripting_1600/Assets/AnotherClass.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnotherClass
+
+{
+	public int shirt;
+	public int socks;
+
+	private int pencil;
+	private int pen;
+
+	public void Clothes (int a, int b)
+		//naming the public section of ints Clothes. There are two public ints, thus a and b.
+	{
+		int solution;
+		solution = a + b;
+		Debug.Log ("Clothes:" + solution);
+		//This is saying that shirts and socks added together make up the Clothes section.
+	}
+
+	private void WritingUtensils (int a, int b)
+		//naming the private section of ints WritingUtensils. Again, there are two ints, thus a and b.
+	{
+		int solution;
+		solution = a + b;
+		Debug.Log ("WritingUtensils:" + solution);
+		//Pencil and pen added up make up the WritingUtensils section.
+	}
+	
+}
+//for some reason Unity is not recognizing this Class. ScopeAndAccessModifiers still works, but this wont.
--- scripting_1600/Assets/ScopeAndAccessModifiers.cs
+++ scripting_1600/Assets/ScopeAndAccessModifiers.cs
@@ -1,15 +1,39 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 
-public class ScopeAndAccessModifiers : MonoBehaviour {
+public class ScopeAndAccessModifiers : MonoBehaviour
+{
+	public int car = 3;
+	//public stuff shows up and is editable in Unity (you don't have to open the script to edit it).
 
-	// Use this for initialization
-	void Start () {
-	
+	private int bus = 0;
+	private int truck = 2;
+	//private stuff doesn't show up. It wouldn't show up anyway, but apparently this is a good practice.
+	//these values are assigned, but never used again because that is what the example did... not sure why... so I decided to add it to the bottom two voids, which made my numbers larger than just the car = 4.
+	private AnotherClass theOtherClass;
+
+	void Start()
+	{
+		car = 4;
+
+		//adding AnotherClass public integers into this Class.
+		theOtherClass = new AnotherClass ();
+		theOtherClass.Clothes (car, theOtherClass.socks);
+		//pulling the integer socks that was in the public Clothes section into this Class.
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+
+	void NewThings (int robot, int radio, int bus, int truck, int car)
+	{
+		int happiness;
+		happiness = robot * radio * bus * truck * car;
+		Debug.Log (happiness);
+		//not quite sure what this is for other than it includes the public integer from before, and it is declaring new integers and using them to equal happiness.
+		//happiness is the integer I decided to use. Basically robot times radio times car equals happiness.
+	}
+
+	void Update ()
+	{
+		Debug.Log ("Car is set to:" + car + bus + truck);
+		//I tested this in Unity, and because I added car and bus and truck, the car is set to 402. It was originally just car, which made the car set to 4. This repeats infinitely in the script... and Im not sure why except that I never told it to stop so... I guess it doesn't.
 	}
 }
