4351f4a7c327d5120389df8cdd1a130330299089
Sadie Fountaine <sadiefountaine@live.com>
2015-10-06 09:10:02 -0600

    Data Types Complete

---
+++ scripting_1600/Assets/DataTypes.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DataTypes : MonoBehaviour
+{
+
+	void Start ()
+	{
+		//Vector3 pos = transform.position;
+		//pos = new Vector3 (0, 5, 0);
+
+
+		Transform tran = transform;
+		tran.position = new Vector3 (0, 5, 0);
+
+	}
+
+}
