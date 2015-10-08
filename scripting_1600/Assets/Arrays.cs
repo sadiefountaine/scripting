2d1010dd3dad8fb4f931c3bcf10cb0fca8ce96f3
Sadie Fountaine <sadiefountaine@live.com>
2015-10-08 09:18:04 -0600

    Arrays Complete

--- scripting_1600/Assets/Arrays.cs
+++ scripting_1600/Assets/Arrays.cs
@@ -3,11 +3,12 @@ using System.Collections;
 
 public class Arrays : MonoBehaviour
 {
-	public GameObject [] characters;
+	public GameObject[] characters;
 
 	void Start ()
 	{
 		characters = GameObject.FindGameObjectsWithTag ("Player");
+
 		for (int i = 0; i < characters.Length; i++) 
 		{
 			Debug.Log ("Character Number "+ i +" is named" +characters[i].name);
