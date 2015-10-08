9192005bf32f8f8b0c6f97cf89ff566338794e2d
Sadie Fountaine <sadiefountaine@live.com>
2015-10-08 09:01:09 -0600

    Arrays Beginning

---
+++ scripting_1600/Assets/Arrays.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Arrays : MonoBehaviour
+{
+	public GameObject [] characters;
+
+	void Start ()
+	{
+		characters = GameObject.FindGameObjectsWithTag ("Player");
+		for (int i = 0; i < characters.Length; i++) 
+		{
+			Debug.Log ("Character Number "+ i +" is named" +characters[i].name);
+		}
+	}
+}
