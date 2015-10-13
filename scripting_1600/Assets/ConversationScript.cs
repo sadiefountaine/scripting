bd6f1250c1c5c9e408031f828148f3ac42c8fe30
Sadie Fountaine <sadiefountaine@live.com>
2015-10-13 09:20:11 -0600

    Conversation Script Complete

---
+++ scripting_1600/Assets/ConversationScript.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ConversationScript : MonoBehaviour 
+{
+	public int greeting = 3;
+	
+	void Greet()
+	{
+		switch (greeting)
+		{
+		case 3:
+			print ("It's nice to meet you!");
+			break;
+		case 2:
+			print ("I don't want to talk to you.");
+			break;
+		case 1:
+			print ("... Why are you talking to me?");
+			break;
+		default:
+			print ("Incorrect greeting.");
+			break;
+		}
+	}
+
+}
